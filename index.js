1adc4bdd294a927fdb738b81c2f214f492b8d5d9
Sushant <sushantbasak73@gmail.com>
2023-05-15 20:58:30 +0530

    feat: Initialize Project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
+});
+
+app.listen(port, () => console.log(`Server started at port ${port}`));

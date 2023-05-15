76831b8abae23304d2226867304d7302bab68f1d
Sushant <sushantbasak73@gmail.com>
2023-05-16 01:08:27 +0530

    fix: Async DB Connection in start

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const api = require("./api");
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
-const app = express();
-const api = require("./api");
+const init = async () => {
+  try {
+    const app = express();
 
-mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const jsonParser = express.json();
+    app.use(jsonParser);
+
+    app.use("/api", api);
+
+    app.get("/", (req, res) => {
+      res.status(200).send({
+        status: "Success",
+        msg: "Api is running",
+      });
+    });
+
+    app.get("/health", (req, res) => {
+      res.status(200).send({ status: "Success", msg: "UP" });
+    });
+
+    app.get("*", function (req, res) {
+      res.status(404).send({
+        status: "Success",
+        msg: "Requested Page does not exist",
+      });
+    });
+
+    return app;
+  } catch (ex) {
+    throw new Error(ex);
+  }
+};
+
+init()
+  .then((app) => app.listen(port))
+  .then(() => {
+    console.log("Server started on port:", port);
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-const jsonParser = express.json();
-app.use(jsonParser);
-
-app.use("/api", api);
-
-app.get("/", (req, res) => {
-  res.status(200).send({
-    status: "Success",
-    msg: "Api is running",
+  .catch((err) => {
+    console.error(err);
   });
-});
-
-app.get("/health", (req, res) => {
-  res.status(200).send({ status: "Success", msg: "UP" });
-});
-
-app.get("*", function (req, res) {
-  res.status(404).send({
-    status: "Success",
-    msg: "Requested Page does not exist",
-  });
-});
-
-app.listen(port, () => console.log(`Server started at port ${port}`));
